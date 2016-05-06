f43b6d3ca93e3922371e9836a1ddcbb201603a3f
John <jhartmann@georgeconsulting.com>
2016-05-06 10:08:56 -0400

    Instead of copying, create a symlink

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-cp .tmux.conf ~/
+# TODO:
+# if file exists
+#   if file is not link
+#     ask user wants to save the config with a '.orig' appended to filename
+
+ln -s $(readlink -f .tmux.conf) ~/.tmux.conf
